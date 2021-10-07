ca81eb6c46da2fbefef5ff96d5bcd5e69d3d8680
32674309 - NJ Morgenrood <Nathan333nm@gmail.com>
2021-10-07 23:31:37 +0200

    RestServiceApplication and GeneralResponse classes added

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/controller/AccountTypeController.java
@@ -0,0 +1,11 @@
+package za.ac.nwu.as.web.sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.as.domain.service.GeneralResponse;
+
+@RestController
+public class AccountTypeController
+{
+
+}
