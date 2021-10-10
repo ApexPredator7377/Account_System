9012f6782c37235ccf37329ddd1210a8b1f9d471
32674309 - NJ Morgenrood <Nathan333nm@gmail.com>
2021-10-10 04:11:20 +0200

    Spring End to End setup complete (payload works, one error causes a connection loss with DB)

---
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/as/translator/AccountTypeTranslator.java
@@ -0,0 +1,9 @@
+package za.ac.nwu.as.translator;
+
+import za.ac.nwu.as.domain.dto.AccountTypeDto;
+
+import java.util.List;
+
+public interface AccountTypeTranslator {
+    List<AccountTypeDto> getAllAccountTypes();
+}
